4d71eacd6795145e250d036db4bed49bdfe8bf6d
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-05-17 23:04:35 +0300

    feat: start solving logger task

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -154,8 +154,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(`${func.name}(${[...args]}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${[...args]}) ends`);
+    return result;
+  };
+  // throw new Error('Not implemented');
 }
 
 
